2304e04f164e97c44b6d7d5be541a601c20433d0
Matija19 <matijagh@gmail.com>
2021-11-01 16:43:09 +0100

    Add files via upload

---
+++ Twitter sentiment to discord.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  1 12:38:13 2021
+
+@author: Matija
+"""
+
+import discord
+import time
+import tweepy
+from Twitter_sentiment import scrape, save_photo
+import nest_asyncio
+nest_asyncio.apply()
+
+# PLUG IN YOUR INFO HERE (PREFERENCES)
+########################################
+# Twitter connection
+# Replace with your credentials
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+# Discord connection
+# Replace with your own connection
+discord_key = ''
+
+# Filenames for saving tweets and tweet sentiment
+tweets_file = 'scrapped_tweets.csv'
+# For sentiment file initialize the csv file by adding header, e.g. Datetime,Sentiment
+sentiment_file = 'sentiment_at_datetime.csv'
+plot_name = 'Sentiment_at_datetime.png'
+
+# Sleep time
+sleep_time = 600
+# Keywords
+keyword = 'bitcoin'
+# Number of tweets to analyze
+numtweet = 1000
+########################################
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    # After running programme type $run into discord server to run
+    if message.content.startswith('$run'):
+        while True:
+            datetime, sentiment = scrape(words=keyword, numtweet=numtweet, api=api, tweets_file=tweets_file,sentiment_file=sentiment_file)
+            save_photo(sentiment_file=sentiment_file, plot_name=plot_name)
+            await message.channel.send(f'Datetime: {datetime}, Sentiment: {sentiment}', file=discord.File(plot_name))
+            time.sleep(sleep_time)
+
+client.run(discord_key)
---
+++ Twitter_sentiment.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 31 13:59:20 2021
+
+@author: Matija
+"""
+
+
+import pandas as pd
+import tweepy
+from nltk.corpus import opinion_lexicon
+from nltk.tokenize import treebank
+from matplotlib import pyplot
+    
+pos_list=set(opinion_lexicon.positive())
+neg_list=set(opinion_lexicon.negative())
+
+tokenizer = treebank.TreebankWordTokenizer()
+
+def sentiment(sentence):
+  senti=0
+  words = [word.lower() for word in tokenizer.tokenize(sentence)]
+  for word in words:
+    if word in pos_list:
+      senti += 1
+    elif word in neg_list:
+      senti -= 1
+  return senti
+
+def scrape(words, numtweet, api, tweets_file, sentiment_file):
+    # Creating DataFrame using pandas
+    db = pd.DataFrame(columns=['username', 'description', 'location', 'following',
+                               'followers', 'totaltweets', 'retweetcount', 'text', 'hashtags',
+                               'created_at'], dtype=object)
+    # We are using .Cursor() to search through twitter for the required tweets.
+    # The number of tweets can be restricted using .items(number of tweets)
+    # print(f'{words} since: {date_since}')
+    tweets = tweepy.Cursor(api.search_tweets, q=words, lang="en",
+                           tweet_mode='extended').items(numtweet)
+    
+    # .Cursor() returns an iterable object. Each item in 
+    # the iterator has various attributes that you can access to 
+    # get information about each tweet
+    list_tweets = [tweet for tweet in tweets]
+      
+    # Counter to maintain Tweet Count
+    i = 1  
+    final_sentiment = 0
+      
+    # we will iterate over each tweet in the list for extracting information about each tweet
+    for tweet in list_tweets:
+        username = tweet.user.screen_name
+        description = tweet.user.description
+        location = tweet.user.location
+        following = tweet.user.friends_count
+        followers = tweet.user.followers_count
+        totaltweets = tweet.user.statuses_count
+        retweetcount = tweet.retweet_count
+        hashtags = tweet.entities['hashtags']
+        created_at = tweet.created_at
+          
+        # Retweets can be distinguished by a retweeted_status attribute,
+        # in case it is an invalid reference, except block will be executed
+        try:
+            text = tweet.retweeted_status.full_text
+        except AttributeError:
+            text = tweet.full_text
+        hashtext = list()
+        for j in range(0, len(hashtags)):
+            hashtext.append(hashtags[j]['text'])
+          
+        # Here we are appending all the extracted information in the DataFrame
+        ith_tweet = [username, description, location, following,
+                     followers, totaltweets, retweetcount, text, hashtext,
+                     created_at]
+        db.loc[len(db)] = ith_tweet
+          
+        # Function call to print tweet data on screen
+        # printtweetdata(i, ith_tweet)
+        final_sentiment += sentiment(ith_tweet[7])
+        i = i+1
+    filename = tweets_file
+      
+    # we will save our database as a CSV file.
+    db.to_csv(filename)
+    
+    #print(f'\n{ith_tweet[9]},{final_sentiment}')
+    filename = sentiment_file
+    with open (filename, 'a') as fd:
+        fd.write(f'\n{ith_tweet[9]},{final_sentiment}')
+    return ith_tweet[9], final_sentiment    
+
+def save_photo(sentiment_file, plot_name):
+    series = pd.read_csv(sentiment_file, header = 0, index_col=0, parse_dates=True)
+    series.plot()
+    pyplot.savefig(plot_name)
